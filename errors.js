06cf2bad426227758d5095461c9cce7763fd8c9f
jesusGZ <j.gr.zurita@gmail.com>
2021-07-14 11:04:21 -0500

    Configuracion inicial del servicio

---
+++ errors.js
@@ -0,0 +1,13 @@
+exports.API_ENDPOINT_NOT_FOUND_ERR = "El punto final de la API no se encuentra";
+
+exports.SERVER_ERR = "Algo salió mal";
+
+exports.AUTH_HEADER_MISSING_ERR = "Falta el encabezado de autenticación";
+
+exports.AUTH_TOKEN_MISSING_ERR = "Falta el token de autenticación";
+
+exports.JWT_DECODE_ERR = "token incorrecto";
+
+exports.USER_NOT_FOUND_ERR = "Usuario no encontrado";
+
+exports.ACCESS_DENIED_ERR = "Acceso denegado para usuario normal";
