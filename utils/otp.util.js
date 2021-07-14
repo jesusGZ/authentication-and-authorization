066971e866cff364646cae126927e23086fb3d0b
jesusGZ <j.gr.zurita@gmail.com>
2021-07-14 11:21:22 -0500

    utils de token y otp

---
+++ utils/otp.util.js
@@ -0,0 +1,25 @@
+const fast2sms = require("fast-two-sms");
+const {FAST2SMS} = require("../config");
+
+exports.generateOTP = (otp_length) => {
+  // Declare una variable de dígitos que almacena todos los dígitos
+  var digits = "4451191234";
+  let OTP = "";
+  for (let i = 0; i < otp_length; i++) {
+    OTP += digits[Math.floor(Math.random() * 10)];
+  }
+  return OTP;
+};
+
+exports.fast2sms = async ({ message, contactNumber }, next) => {
+  try {
+    const res = await fast2sms.sendMessage({
+      authorization: FAST2SMS,
+      message,
+      numbers: [contactNumber],
+    });
+    console.log(res);
+  } catch (error) {
+    next(error);
+  }
+};
